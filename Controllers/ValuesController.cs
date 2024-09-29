49018dae86472230e13655884ab630ec21e0fc2d
sahulhameed.s <sahulhameed.s@bthinq.com>
2024-09-29 19:55:36 +0530

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize(Roles ="Admin")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2", "value3", "value4", "value5" };
+        }
+    }
+}
